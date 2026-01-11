b6e3090a72b17b630cce461338f151a58ee00739
Riku Oya <uqroya@uq.edu.au>
2026-01-11 22:31:10 +1000

    test code with pi pico

---
+++ 02 test code/01 hello world/main.py
@@ -0,0 +1,9 @@
+from machine import Pin
+from time import sleep
+led = Pin('LED', Pin.OUT)
+n = 0;
+while True:
+    led.toggle()
+    print("13 x {} = {}".format(n, 13*n)) # print the thirteen-times table
+    n = n + 1
+    sleep(0.5)
